8facb549eac699f651642b5e02297107ad32f292
jBoscW <jbd393@nyu.edu>
2025-02-11 10:36:13 -0300

    Do exercise 5

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...nums) {
+    for (let i = 0; i < nums.length; i++) {
+        let arg = nums[i]
+        while (arr.includes(arg)) {
+            const pos = arr.indexOf(arg);
+            arr.splice(pos, 1);
+        }
+    }
+    return arr;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
