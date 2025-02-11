a836b1c68a20ada1df75de637334ac20556c5b08
jBoscW <jbd393@nyu.edu>
2025-02-11 11:35:16 -0300

    Do ex. 7 and FINISH... for now

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fare) {
+  let celsius = (fare - 32) * 5 / 9;
+  celsius = Math.round(celsius * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let fare = (cel * 9 / 5) + 32;
+  fare = Math.round(fare * 10) / 10 
+  return fare;
 };
 
 // Do not edit below this line
