c9a2f64fd1789937c4e8358e772c8d9f551d6154
jBoscW <jbd393@nyu.edu>
2025-02-11 11:25:40 -0300

    Do ex. 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(yr) {
+    let leap = false
+    if (yr % 4 === 0) {
+        if (yr % 100 === 0 && yr % 400 !== 0)
+            return leap;
+        else
+            leap = true;
+    }
+    return leap;
 };
 
 // Do not edit below this line
