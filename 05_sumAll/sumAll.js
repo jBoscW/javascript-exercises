ff884cb4423eb5de285048324d608d754d961864
jBoscW <jbd393@nyu.edu>
2025-02-11 11:09:59 -0300

    Do ex. 5 fr this time

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    args = Array.from(arguments)
+    if (args.every(n => n >= 0) 
+        && args.every(n => Number.isInteger(n))) {
+        if (num2 < num1) {
+            let temp = num1;
+            num1 = num2;
+            num2 = temp;
+        }
 
+        let sum = 0;
+        for (let i = num1; i <= num2; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    return 'ERROR';
 };
 
 // Do not edit below this line
