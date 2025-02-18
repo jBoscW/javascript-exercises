bebce3bd1f2fd08e4a1a3c5a9992f5445af27bab
jBoscW <jbd393@nyu.edu>
2025-02-18 19:36:19 -0300

    Do exercise 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  // if (arr.length === 0)
+  //   return 0;
+
+	return arr.reduce((sum, i) => sum + i, 0);
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  // if (arr.length === 0)
+  //   return 0;
 
+	return arr.reduce((sum, i) => sum * i);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let sum = 1;
+  for (let i = 1; i <= a; i++) 
+    sum *= i;
+  return sum;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    function isLetter(c) {
+        return (c.toLowerCase() != c.toUpperCase()
+                || c in [...Array(10).keys()]);
+    };
 
+    const strC = str
+                            .split('')
+                            .filter((c) => isLetter(c))
+                            .join('')
+                            .toLowerCase();
+
+    const half = Math.floor(strC.length / 2) + 1;
+    for (let i = 0; i < half; i++) {
+        if (strC[i] !== strC.at(-1 - i))
+            return false;
+    }
+    return true;
 };
 
+
+// palindromes('r3ace3car')
 // Do not edit below this line
 module.exports = palindromes;
